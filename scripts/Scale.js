d2e18b4f5538abd8d9b9a6bccde0935ebd686bf0
web-project-dev <temki2004@gmail.com>
2025-06-23 10:13:00 +0300

    Add files via upload

---
+++ scripts/Scale.js
@@ -0,0 +1,12 @@
+const imgScale = document.querySelector('.home__information-certificate-img')
+const overlay = document.querySelector('.overlay')
+
+imgScale.onclick = function() {
+    imgScale.classList.add('is-active');
+    overlay.style.display = 'block';
+}
+
+overlay.onclick = function() {
+    overlay.style.display = 'none';
+    imgScale.classList.remove('is-active')
+}
--- scripts/Scroll.js
+++ scripts/Scroll.js
@@ -2,10 +2,12 @@ const nav1 = document.querySelectorAll('.nav-home');
 const nav2 = document.querySelectorAll('.nav-services');
 const nav3 = document.querySelectorAll('.nav-clients');
 const nav4 = document.querySelectorAll('.nav-contacts');
+const burgerModal = document.querySelector('.header__overlay')
 
 nav1.forEach(item => {
     item.onclick = function() {
         document.querySelector('.home').scrollIntoView({ behavior: 'smooth', block: 'end' });
+        burgerModal.classList.remove('is-active')
     };
 })
 
@@ -13,17 +15,20 @@ nav1.forEach(item => {
 nav2.forEach(item => {
     item.onclick = function() {
         document.querySelector('.services').scrollIntoView({ behavior: 'smooth', block: 'center' });
+        burgerModal.classList.remove('is-active')
     };
 })
 
 nav3.forEach(item => {
     item.onclick = function() {
         document.querySelector('.clients').scrollIntoView({ behavior: 'smooth', block: 'center' });
+        burgerModal.classList.remove('is-active')
     };
 })
 
 nav4.forEach(item => {
     item.onclick = function() {
         document.querySelector('.contacts').scrollIntoView({ behavior: 'smooth', block: 'end' });
+        burgerModal.classList.remove('is-active')
     };
 })
