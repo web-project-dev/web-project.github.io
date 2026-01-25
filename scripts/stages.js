4e16a8f639b6638bde4cf25a2118bb32fb266576
web-project-dev <temki2004@gmail.com>
2026-01-25 14:52:16 +0300

    Add files via upload

--- scripts/stages.js
+++ scripts/stages.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const listItems = document.querySelectorAll('.stages__list-item');
+    const arrows = document.querySelectorAll('.stages-arrow'); 
     
-    listItems.forEach(item => {
+    listItems.forEach((item, index) => {
         item.addEventListener('click', function() {
             // Закрываем все остальные элементы
-            listItems.forEach(otherItem => {
+            listItems.forEach((otherItem, otherIndex) => {
                 if (otherItem !== this) {
                     const otherHidden = otherItem.querySelector('.stages__list-item-hidden');
                     otherHidden.classList.remove('active');
                     otherItem.classList.remove('active');
+                    arrows[otherIndex]?.classList.remove('active'); 
                 }
             });
             
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const hiddenText = this.querySelector('.stages__list-item-hidden');
             hiddenText.classList.toggle('active');
             this.classList.toggle('active');
+            arrows[index]?.classList.toggle('active'); 
         });
     });
 });
